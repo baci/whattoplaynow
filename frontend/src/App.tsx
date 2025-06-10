8806c2bdfcb284ae78bba037ee7a75047ea488e4
Till Riemer <till.riemer@gmx.de>
2025-06-10 23:51:22 +0200

    Frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+  return (
+    <div className="bg-red-500 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl">Test</h1>
+    </div>
+  );
+}
